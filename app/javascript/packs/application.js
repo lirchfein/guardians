f26bbd099c1f7b2144d0171c105d48b76d2b1839
Paul Jelkmann <paul.jelkmann@gmail.com>
2018-11-20 19:05:26 +0100

    finished dynamic book button

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -2,3 +2,6 @@ import "bootstrap";
 
 import { addEventListenerforAccordionTab } from '../layouts/accordion';
 addEventListenerforAccordionTab();
+
+import { disableButton } from '../guards/button_unavailable';
+disableButton();
