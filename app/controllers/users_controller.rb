b9449573df664ba5ccf4a394764aab3c28afdebc
Wiebke Frey <frey.wiebke@googlemail.com>
2018-11-21 23:12:27 +0100

    Add show more & less button + functionality

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,9 @@
 class UsersController < ApplicationController
   def show
     @user = User.find(params[:id])
+    @open_bookings = @user.bookings.reject(&:completed)
+    @left_bookings_space = 5 - @open_bookings.length # used to show not too many bookings
+    @completed_bookings = @user.bookings.select(&:completed)
     authorize @user
   end
 end
--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -4,9 +4,11 @@ import { addEventListenerforAccordionTab } from '../layouts/accordion';
 import { addEventListenerToEmptyTick } from './profile';
 import { addEventListenerToTabs } from './profile';
 import { disableButton } from '../guards/button_unavailable';
+import { addEventListenerShowMore } from './profile';
 
 addEventListenerforAccordionTab();
 addEventListenerToEmptyTick();
 addEventListenerToTabs();
 disableButton();
+addEventListenerShowMore();
 
